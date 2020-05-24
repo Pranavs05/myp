60ba86c770759996fc980590bf22f28ce560fdf7
Pranav Shirke <pranavshirke@Pranavs-MacBook-Pro.local>
2020-05-24 15:23:13 -0400

    Updates

---
+++ precache-manifest.b3d41cfc3e7dfafaa946055287a7512b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf3952a0e3e6388a2f6b5097b0d2b07f",
+    "url": "/Pranavs05/myp/index.html"
+  },
+  {
+    "revision": "ed1753109e957c7a9cfd",
+    "url": "/Pranavs05/myp/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "233f7cf007365e48cf52",
+    "url": "/Pranavs05/myp/static/css/main.5b39e0f2.chunk.css"
+  },
+  {
+    "revision": "ed1753109e957c7a9cfd",
+    "url": "/Pranavs05/myp/static/js/2.a1cd7cfa.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/Pranavs05/myp/static/js/2.a1cd7cfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "233f7cf007365e48cf52",
+    "url": "/Pranavs05/myp/static/js/main.7567cff9.chunk.js"
+  },
+  {
+    "revision": "8bf4ca7387aed12ec1f7",
+    "url": "/Pranavs05/myp/static/js/runtime-main.6e02c33f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Pranavs05/myp/static/media/logo.5d5d9eef.svg"
+  }
+]);
